9eeb04e78d69d8b2e2eb7002326784915ef44f52
Lucy <ll7@princeton.edu>
2016-06-12 01:10:19 +0200

    Web extension, needs CORS

---
+++ popup.js
@@ -0,0 +1,24 @@
+function paideiafy(language) {
+  chrome.tabs.insertCSS({
+    file: 'contentscripts/my.css'
+  });
+  chrome.tabs.executeScript({
+    file: 'contentscripts/paideiafy.js'
+  });
+
+  var runScript;
+  if (language === 'latin') {
+    runScript = 'runPaideiaChromium("latin")';
+  } else {
+    runScript = 'runPaideiaChromium("greek")';
+  }
+
+  chrome.tabs.executeScript({
+    code: runScript,
+  });
+}
+
+document.getElementById('paideia-latin').addEventListener('click', function() { paideiafy('latin') });
+document.getElementById('paideia-greek').addEventListener('click', function() { paideiafy('greek') });
+
+// should also have scrolling capacity for large entries.
