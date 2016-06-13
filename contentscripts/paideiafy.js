8cf9e7607ed9b68aa1d0412ea847fd9468a55059
Lucy <ll7@princeton.edu>
2016-06-13 16:37:35 +0200

    Smaller font, prettier buttons

--- contentscripts/paideiafy.js
+++ contentscripts/paideiafy.js
@@ -8,7 +8,7 @@
 ******** */
 function anotherDictionary (word) {
   return '<p>Try this word in another dictionary: </p>' +
-    '<ul>' +
+    '<ul class="another-dict">' +
       '<li><a target="_blank" href="http://logeion.uchicago.edu/index.html#'+ word + '">Logeion</a></li>' +
       '<li><a target="_blank" href="http://www.perseus.tufts.edu/hopper/resolveform?type=exact&lookup=&lang=greek">Perseus LSJ</a></li>' +
     '</ul>'
@@ -40,7 +40,7 @@ function insertDiv(child) {
 function manualSearch(word) {
   insertDiv(
     '<div class="container" id="paideia-panel"><div class="row">' +
-    '<h2 style="margin:0;">Sorry!</h2> ' +
+    '<h2 style="margin:0;font-size:16px;font-weight:bold;">Sorry!</h2> ' +
     '<p>We couldn\'t find any results for this entry.</p>' +
     anotherDictionary(word) +
     '<p>Or try typing the word manually:</p>' +
@@ -65,7 +65,7 @@ function manualSearch(word) {
 }
 
 function paidieaify(word, language) {
-  var thanks = '<hr style="margin-top: 2em;" /><footer style="font-size:10px; text-align: right;">Morphology provided by Morpheus from the <a href="http://www.perseus.tufts.edu/hopper/">Perseus Digital Library</a> at Tufts University.</footer>';
+  var thanks = '<hr style="margin-top: 2em;" /><footer style="font-size:10px; text-align: left;">Morphology provided by Morpheus from the <a href="http://www.perseus.tufts.edu/hopper/">Perseus Digital Library</a> at Tufts University.</footer>';
   var langCode = 'la'; // latin by default
   if (language == 'greek') {
     langCode = 'greek';
@@ -85,20 +85,17 @@ function paidieaify(word, language) {
 
   $.ajax(settings).done(function (response) {
     var toReturn = (response === null) ? info.selectionText : response;
-
     // create jquery object from HTML
     var perseus = $('<div/>').html(toReturn).contents();
           lemma = perseus.find('.lemma');
           resultFound = perseus.find('.lemma').html(); // will be undefined if perseus finds no results
-
-    console.log(resultFound);
     if (resultFound) {
       var header = lemma.find('.lemma_header').prop('outerHTML');
             table = lemma.find('table').addClass('paideia-table').prop('outerHTML');
       insertDiv('<div id="paideia-panel">' + header + "<br />" + table + anotherDictionary(word) + thanks + '</div>');
       $('#paideia-panel').click(rmPanel);
     } else {
-      manualSearch(word)
+      manualSearch(word);
     }
   }).fail(function() {
     alert( "error" );
@@ -108,6 +105,6 @@ function paidieaify(word, language) {
 function runPaideiaChromium(language) {
   document.body.addEventListener('dblclick', function(info) {
     console.log("received doubleclick");
-    paidieaify(window.getSelection().toString(), language)
+    paidieaify(window.getSelection().toString(), language);
   });
 }
