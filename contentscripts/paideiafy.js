84465ee17d4b84f60d22f0b672aec817ed02d26f
Lucy <ll7@princeton.edu>
2016-06-24 15:02:34 +0200

    Word wrap word crap

--- contentscripts/paideiafy.js
+++ contentscripts/paideiafy.js
@@ -21,10 +21,12 @@ var saved = "";
 
 function quizlet() {
   insertDiv('<div id="paideia-panel"><b>Just copy and paste the text below into the import space when creating a Quizlet set.</b><br>' + 
-    '<div id="vocab"><pre style="font-size: 12px; font-family: Geneva, sans-serif; text-align: left;">' + saved + '</pre></div>' + 
+    '<div id="vocab"><pre style="font-size: 12px; font-family: Geneva, sans-serif; text-align: left; '
+    + 'white-space: pre-wrap; white-space: -moz-pre-wrap; white-space: -pre-wrap; '
+    + 'white-space: -o-pre-wrap; word-wrap: break-word;">' + saved + '</pre></div>' + 
     '<button id="copy">Copy vocabulary to clipboard!</button>' + 
     '<button id="go-quizlet">Open Quizlet in new tab</button>' + 
-    '<button id="remove">Close</button></div>');
+    '<button id="remove">Close</button><br></div>');
   $('#remove').click(rmPanel);
   document.getElementById('copy').addEventListener('click', function() {executeCopy(saved);});
   document.getElementById('go-quizlet').addEventListener('click', function() {
@@ -47,7 +49,7 @@ function anotherDictionary (word) {
     '<ul class="another-dict">' +
       '<li><a target="_blank" href="http://logeion.uchicago.edu/index.html#'+ word + '">Logeion</a></li>' +
       '<li><a target="_blank" href="http://www.perseus.tufts.edu/hopper/resolveform?type=exact&lookup=' + 
-        word + '&lang=' + lang + '">Perseus LSJ</a></li>' + 
+        word + '&lang=' + lang + '">Perseus</a></li>' + 
     '</ul>'
 };
 
@@ -60,8 +62,8 @@ function insertDiv(child) {
   var div = document.createElement('div');
   div.setAttribute('id', 'paideia-panel');
   div.setAttribute('style', 'position: fixed; top: 1em; right: 1em; padding: 10px 20px; '
-    +'border: 1px solid #007095; border-radius: 2em; max-width: 34em;'
-    +' word-wrap: break-word; background-color: aliceblue; z-index:999;');
+    +'border: 1px solid #007095; border-radius: 2em; max-width: 34em; max-height: 40em; '
+    + 'overflow-y: scroll; word-wrap: break-word; background-color: aliceblue; z-index:999;');
 
   rmPanel()
 
@@ -101,7 +103,7 @@ function manualSearch(word) {
 
 function parseAjax(word, toReturn) {
   console.log("parsing ajax");
-  var thanks = '<hr style="margin-top: 2em;" /><footer style="font-size:10px; text-align: left;">Morphology provided by Morpheus from the <a href="http://www.perseus.tufts.edu/hopper/">Perseus Digital Library</a> at Tufts University.</footer>';
+  var thanks = '<hr style="margin-top: 2em;" /><footer style="font-size:10px; text-align: left;"><img src="' + chrome.extension.getURL("paideia.png") +'" alt="Paideia Icon" style="width:5em;height:5em;float:left;">Chromium was developed by the <a href="http://paideiainstitute.org">Paideia Institute for Humanistic Study</a>.<br>Morphology provided by Morpheus from the <a href="http://www.perseus.tufts.edu/hopper/">Perseus Digital Library</a> at Tufts University. </footer>';
   var perseus = $('<div/>').html(toReturn).contents();
   lemma = perseus.find('.lemma');
   resultFound = perseus.find('.lemma').html(); // will be undefined if perseus finds no results
@@ -112,16 +114,13 @@ function parseAjax(word, toReturn) {
     saved += word_saved + '\t' + def.trim() + '\n';
     console.log(saved);
     table = lemma.find('table').addClass('paideia-table').prop('outerHTML');
-    insertDiv('<div id="paideia-panel"><button id="remove" style="float: right;">X</button>' + header + "<br />" + table + anotherDictionary(word) + thanks + '</div>');
+    insertDiv('<div id="paideia-panel"><button id="remove" style="float: right;">X</button>' + header + "<br /><h4>Possible Forms:</h4>" + table + anotherDictionary(word) + thanks + '</div>');
     $('#remove').click(rmPanel);
   } 
   else manualSearch(word);
 }
 
 function paidieaify(word, language) {
-  // var langCode = 'la'; // latin by default
-  // if (language == 'greek') langCode = 'greek';
-
   console.log("before ajax");
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
