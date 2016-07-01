58c177171177e09283f63e7538d6601f5c5ce3e4
Lucy <ll7@princeton.edu>
2016-07-01 14:30:16 +0200

    Pretty

--- contentscripts/paideiafy.js
+++ contentscripts/paideiafy.js
@@ -20,10 +20,8 @@ var lang;
 var saved = "";
 
 function quizlet() {
-  insertDiv('<div id="paideia-panel"><b>Just copy and paste the text below into the import space when creating a Quizlet set.</b><br>' + 
-    '<div id="vocab"><pre style="font-size: 12px; font-family: Geneva, sans-serif; text-align: left; '
-    + 'white-space: pre-wrap; white-space: -moz-pre-wrap; white-space: -pre-wrap; '
-    + 'white-space: -o-pre-wrap; word-wrap: break-word;">' + saved + '</pre></div>' + 
+  insertDiv('<div id="paideia-panel"><h2 style="font-size:14px;font-weight:bold;margin:0;font-family:inherit;">Just copy and paste the text below into the import space when creating a Quizlet set.</h2>' + 
+    '<div id="vocab"><pre>' + saved + '</pre></div>' + 
     '<button id="copy">Copy vocabulary to clipboard!</button>' + 
     '<button id="go-quizlet">Open Quizlet in new tab</button>' + 
     '<button id="remove">Close</button><br></div>');
@@ -45,12 +43,14 @@ function executeCopy(text) {
 }
 
 function anotherDictionary (word) {
-  return '<p>Try this word in another dictionary: </p>' +
-    '<ul class="another-dict">' +
-      '<li><a target="_blank" href="http://logeion.uchicago.edu/index.html#'+ word + '">Logeion</a></li>' +
-      '<li><a target="_blank" href="http://www.perseus.tufts.edu/hopper/resolveform?type=exact&lookup=' + 
-        word + '&lang=' + lang + '">Perseus</a></li>' + 
-    '</ul>'
+  return '<div id="another-div"><h5 style="padding-bottom:0.7em;"">Try this word in another dictionary: </h5>' + 
+  '<div class="another-dict">' +
+  '<img src="' + chrome.extension.getURL("logeion.jpeg") + '" alt="Logeion Icon" style="width:0.7em;height:0.7em;">' +
+    '<h3><a target="_blank" href="http://logeion.uchicago.edu/index.html#'+ word + '">Logeion</a></h3>' + 
+    '<img src="' + chrome.extension.getURL("perseus.jpeg") + '" alt="Perseus Icon" style="width:0.7em;height:0.7em;">' +
+    '<h3><a target="_blank" href="http://www.perseus.tufts.edu/hopper/resolveform?type=exact&lookup=' + 
+      word + '&lang=' + lang + '">Perseus</a></h3>' + 
+  '</div></div>'
 };
 
 function rmPanel() {
@@ -62,7 +62,7 @@ function insertDiv(child) {
   var div = document.createElement('div');
   div.setAttribute('id', 'paideia-panel');
   div.setAttribute('style', 'position: fixed; top: 1em; right: 1em; padding: 10px 20px; '
-    +'border: 1px solid #007095; border-radius: 2em; max-width: 34em; max-height: 40em; '
+    +'border: 1px solid #007095; border-radius: 1em; width: 35%; max-height: 50%; '
     + 'overflow-y: scroll; word-wrap: break-word; background-color: aliceblue; z-index:999;');
 
   rmPanel()
@@ -77,10 +77,12 @@ function insertDiv(child) {
 function manualSearch(word) {
   insertDiv(
     '<div class="container" id="paideia-panel"><div class="row">' +
-    '<h2 style="margin:0;font-size:16px;font-weight:bold;">Sorry!</h2> ' +
-    '<p>We couldn\'t find any results for this entry.</p>' +
-    anotherDictionary(word) +
-    '<p>Or try typing the word manually:</p>' +
+    '<p style="text-align: center; font-size: 16px; font-weight: bold;">Sorry!</p> ' +
+    '<h5 style="padding-bottom:0.7em;">We couldn\'t find any results for this entry. Try this word in another dictionary:</h5>' +
+    '<h3><a target="_blank" href="http://logeion.uchicago.edu/index.html#'+ word + '">Logeion</a></h3>' + 
+    '<h3><a target="_blank" href="http://www.perseus.tufts.edu/hopper/resolveform?type=exact&lookup=' + 
+      word + '&lang=' + lang + '">Perseus</a></h3><br>' + 
+    '<h5>Or try typing the word manually:</h5>' +
     '</div><div class="row">' +
     '<div class="col-xs-6 col-xs-offset-3 paideia-input">' +
     '  <input type="text" id="manual-paideia-entry" class="form-control" placeholder="type your word here...">' +
@@ -88,9 +90,7 @@ function manualSearch(word) {
     '  <div style="text-align:center;">' +
     '    <button class="paideia-button" type="submit" id="manual-paideia-search">Search</button>' +
     '    <button class="paideia-button" id="cancel-paideia">Cancel</button>' +
-    '  </div>' +
-    '</div>' +
-    '</div></div>'
+    '  </div>' + '</div>' + '</div></div>'
   );
   $('#manual-paideia-search').click(function() {
     var manualPaideiaEntry = $('#manual-paideia-entry').val();
@@ -103,7 +103,7 @@ function manualSearch(word) {
 
 function parseAjax(word, toReturn) {
   console.log("parsing ajax");
-  var thanks = '<hr style="margin-top: 2em;" /><footer style="font-size:10px; text-align: left;"><img src="' + chrome.extension.getURL("paideia.png") +'" alt="Paideia Icon" style="width:5em;height:5em;float:left;">Chromium was developed by the <a href="http://paideiainstitute.org">Paideia Institute for Humanistic Study</a>.<br>Morphology provided by Morpheus from the <a href="http://www.perseus.tufts.edu/hopper/">Perseus Digital Library</a> at Tufts University. </footer>';
+  var thanks = '<hr style="margin-top: 2em;" /><footer><img src="' + chrome.extension.getURL("paideia.png") + '" alt="Paideia Icon" style="width:5em;height:5em;float:left;">Chromium was developed by the <a href="http://paideiainstitute.org">Paideia Institute for Humanistic Study</a>.<br>Morphology provided by Morpheus from the <a href="http://www.perseus.tufts.edu/hopper/">Perseus Digital Library</a> at Tufts University.</footer>';
   var perseus = $('<div/>').html(toReturn).contents();
   lemma = perseus.find('.lemma');
   resultFound = perseus.find('.lemma').html(); // will be undefined if perseus finds no results
@@ -114,7 +114,15 @@ function parseAjax(word, toReturn) {
     saved += word_saved + '\t' + def.trim() + '\n';
     console.log(saved);
     table = lemma.find('table').addClass('paideia-table').prop('outerHTML');
-    insertDiv('<div id="paideia-panel"><button id="remove" style="float: right;">X</button>' + header + "<br /><h4>Possible Forms:</h4>" + table + anotherDictionary(word) + thanks + '</div>');
+    insertDiv('<div id="paideia-panel"><h4 id="remove" style="float: right; cursor:pointer; margin-top: 0px;">X</h4>' + anotherDictionary(word) + 
+      header + table + thanks + '</div>');
+
+    var thead = document.getElementsByClassName("paideia-table")[0].createTHead();
+    var row = thead.insertRow();
+    var cell1 = row.insertCell();
+    var cell2 = row.insertCell();
+    cell1.innerHTML = "Form";
+    cell2.innerHTML = "Morphology";
     $('#remove').click(rmPanel);
   } 
   else manualSearch(word);
